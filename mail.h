cb2a772eb2801ae0f2d58fb42f443fdd3522f1f6
kri111 <krishna.adettiwar@colorado.edu>
2016-04-26 21:48:13 -0600

    Create mail.h

---
+++ mail.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+  string name, address
+  cout<<"Please Enter your name: "<<endl;
+  cin>>name;
+  cout<<"Please Enter your address: "<<endl;
+  cin>>adress;
+  cout<<"Please enter your city: "endl;
+  cin >>city;
+  
+ string cities[]={"Los Angeles", "Phoenix", "Denver", "Dallas", "St. Louis", "Chicago", "Atlanta", "Washington, D.C.", "New York", "Boston", "NULL"};
+ 
+
+
+}
